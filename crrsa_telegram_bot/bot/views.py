9bd4b653e9a03c198185ed326566ed1afdb9b0f0
Biruk-G3 <byafet3@gmail.com>
2026-03-04 21:30:33 +0300

    webhook

--- crrsa_telegram_bot/bot/views.py
+++ crrsa_telegram_bot/bot/views.py
@@ -10,24 +10,33 @@ from .ai import ask_ai
 def home(request):
     return HttpResponse("Telegram bot is running")
 
-@csrf_exempt
 def webhook(request):
+    print("Webhook hit")
+
     if request.method == "POST":
-        data = json.loads(request.body)
-
-        # Telegram message
-        message = data.get("message")
-        if message:
-            chat_id = message["chat"]["id"]
-            text = message.get("text", "")
-
-            # ask AI
-            reply = ask_ai(text)
-
-            # send reply back to Telegram
-            
-            token = os.environ.get("TELEGRAM_BOT_TOKEN")
-            url = f"https://api.telegram.org/bot{8664683565:AAHeE1QgJB9C0UgPinBGi2hLfxkUojTh01s}/sendMessage"
-            requests.post(url, json={"chat_id": chat_id, "text": reply})
+        print("POST received")
+        print(request.body)
 
     return JsonResponse({"status": "ok"})
+
+# @csrf_exempt
+# def webhook(request):
+#     if request.method == "POST":
+#         data = json.loads(request.body)
+
+#         # Telegram message
+#         message = data.get("message")
+#         if message:
+#             chat_id = message["chat"]["id"]
+#             text = message.get("text", "")
+
+#             # ask AI
+#             reply = ask_ai(text)
+
+#             # send reply back to Telegram
+            
+#             token = os.environ.get("TELEGRAM_BOT_TOKEN")
+#             url = f"https://api.telegram.org/bot{8664683565:AAHeE1QgJB9C0UgPinBGi2hLfxkUojTh01s}/sendMessage"
+#             requests.post(url, json={"chat_id": chat_id, "text": reply})
+
+#     return JsonResponse({"status": "ok"})
