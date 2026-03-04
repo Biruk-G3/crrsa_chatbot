5c37e4c8c4a124038676925f78eca7d4c4bfc85f
Biruk-G3 <byafet3@gmail.com>
2026-03-04 19:47:05 +0300

    create bot app with webhook

--- crrsa_telegram_bot/bot/views.py
+++ crrsa_telegram_bot/bot/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
-# Create your views here.
+@csrf_exempt
+def webhook(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        print(data)  # for testing
+    return JsonResponse({"status": "ok"})
