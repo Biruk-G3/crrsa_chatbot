61b2b409672762f40d972389c59be991de171a01
Biruk-G3 <byafet3@gmail.com>
2026-03-06 14:50:38 +0300

    improving responce

--- crrsa_telegram_bot/bot/ai.py
+++ crrsa_telegram_bot/bot/ai.py
@@ -4,6 +4,10 @@ import os
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
 def ask_ai(question):
+
+    if not GEMINI_API_KEY:
+        return "Gemini API key is missing"
+
     url = "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent"
 
     headers = {
@@ -26,9 +30,14 @@ def ask_ai(question):
         json=data
     )
 
+    print("Status code:", response.status_code)
+
     result = response.json()
 
     print("Full API response:", result)
+
+    if response.status_code != 200:
+        return "AI request failed"
 
     try:
         return result["candidates"][0]["content"]["parts"][0]["text"]
